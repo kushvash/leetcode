4068b3f36af646c4af525e9522597a44010bab11
Kush <156246549+kushvash@users.noreply.github.com>
2025-03-23 01:51:15 -0700

    Time: 1262 ms (8.90%) | Memory: 295.6 MB (8.63%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-        int left=0, mid=0, right, n=nums.size()-1;
+        sort(nums.begin(), nums.end());
+        int start, end, n=nums.size();
         vector<vector<int>> ans;
-        set<vector<int>> set;
-        
-        right=n;
+        set<vector<int>> helper;
 
-        while(left<nums.size()-1){
-            if(nums[left]>0){
+        for(int i=0; i<n; i++) {
+            if(nums[i]>0){
                 break;
             }
+            start=i+1;
+            end=n-1;
 
-            mid=left+1;
-            right=n;
-            while(mid<right){
-                if(nums[left]+nums[mid]+nums[right]==0){
-                    set.insert({nums[left], nums[mid], nums[right]});
-                    right--;
-                    mid++;
-                }else if(nums[left]+nums[mid]+nums[right]>0){
-                    right--;
-                }else{
-                    mid++;
+            while(start<end) {
+                if(nums[i]+nums[start]+nums[end]==0){
+                    helper.insert({nums[i], nums[start], nums[end]});
+                    start++;
+                    end--;
+                }
+                else if(nums[i]+nums[start]+nums[end]>0){
+                    end--;
+                }else {
+                    start++;
                 }
             }
-            left++;
         }
-        for(auto it : set){
+
+        for(auto it: helper) {
             ans.push_back(it);
         }
+
+
         return ans;
     }
 };
