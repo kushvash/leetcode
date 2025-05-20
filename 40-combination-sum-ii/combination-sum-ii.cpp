339221e4301dda3f786477afc2ea3cbb5909b77e
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-19 23:50:58 -0700

    Time: 121 ms (5.07%) | Memory: 41.7 MB (5.02%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void combinationSumHelper(vector<int>& candidates, int start, vector<int> currSet, int target, vector<vector<int>>& res){
+        if(target==0){
+            res.push_back(currSet);
+            return;
+        }
+        if(target<0 || start==candidates.size()){
+            return;
+        }
+
+        //excluding element
+        int i=start;
+        while(i<candidates.size() && (i==start || candidates[i]==candidates[i-1])){
+            i++;
+        }
+        combinationSumHelper(candidates, i, currSet, target, res);
+        
+
+        //including element
+        currSet.push_back(candidates[start]);
+        combinationSumHelper(candidates, start+1, currSet, target-candidates[start], res);
+
+        return;
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> res;
+
+        combinationSumHelper(candidates, 0, {}, target, res);
+
+        return res;
+
+    }
+};
