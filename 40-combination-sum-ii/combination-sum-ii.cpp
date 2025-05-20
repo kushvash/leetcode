0b458cd79de6babed6896f688357d2c1023e6017
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-20 00:03:10 -0700

    Time: 29 ms (7.23%) | Memory: 20.2 MB (7.82%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -9,17 +9,26 @@ public:
             return;
         }
 
-        //excluding element
-        int i=start;
-        while(i<candidates.size() && (i==start || candidates[i]==candidates[i-1])){
-            i++;
-        }
-        combinationSumHelper(candidates, i, currSet, target, res);
+        // //excluding element
+        // int i=start;
+        // while(i<candidates.size() && (i==start || candidates[i]==candidates[i-1])){
+        //     i++;
+        // }
+        // combinationSumHelper(candidates, i, currSet, target, res);
         
 
-        //including element
-        currSet.push_back(candidates[start]);
-        combinationSumHelper(candidates, start+1, currSet, target-candidates[start], res);
+        // //including element
+        // currSet.push_back(candidates[start]);
+        // combinationSumHelper(candidates, start+1, currSet, target-candidates[start], res);
+
+        for(int i=start; i<candidates.size(); i++){
+            if(i>0 && candidates[i]==candidates[i-1] && (i-1)>=start){
+                continue;
+            }
+            currSet.push_back(candidates[i]);
+            combinationSumHelper(candidates, i+1, currSet, target-candidates[i], res);
+            currSet.pop_back();
+        }
 
         return;
     }
@@ -27,7 +36,6 @@ public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         vector<vector<int>> res;
-
         combinationSumHelper(candidates, 0, {}, target, res);
 
         return res;
