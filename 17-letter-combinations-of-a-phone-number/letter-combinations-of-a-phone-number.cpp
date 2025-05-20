8d90a6d71bc31683bf84cfefd6ae2ecd0543c383
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-20 00:20:34 -0700

    Time: 0 ms (100.00%) | Memory: 9.5 MB (25.01%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void letterCombinationsHelper(string digits, int start, string currSet, vector<string>& res) {
+        if(start==digits.size()){
+            if(currSet.size()>0){
+                res.push_back(currSet);
+            }
+            return;
+        }
+
+        for(int i=0; i<dict[digits[start]].size(); i++) {
+            currSet.push_back(dict[digits[start]][i]);
+            letterCombinationsHelper(digits, start+1, currSet, res);
+            currSet.pop_back();        
+        }
+
+        return;
+    }
+    
+    unordered_map<char, string> dict = {
+        {'2' , "abc"},
+        {'3' , "def"},
+        {'4' , "ghi"},
+        {'5' , "jkl"},
+        {'6' , "mno"},
+        {'7' , "pqrs"},
+        {'8' , "tuv"},
+        {'9' , "wxyz"}
+    };
+    
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        letterCombinationsHelper(digits, 0, {}, res);
+
+        return res;
+    }
+};
