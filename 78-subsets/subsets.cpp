d43c40a018a43a5f449d9603840485cf359a90ef
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-20 18:07:03 -0700

    Time: 6 ms (5.94%) | Memory: 15.6 MB (20.15%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void subsetsHelper(vector<int>& nums, vector<int> currSet , int start, vector<vector<int>>& res) {
+        // base case
+        if(start==nums.size()) {
+            res.push_back(currSet);
+            return;
+        }
+
+        // excluding current
+        subsetsHelper(nums, currSet, start+1, res);
+
+        // including current
+        currSet.push_back(nums[start]);
+
+        subsetsHelper(nums, currSet, start+1, res);
+
+        currSet.pop_back();
+
+        return;
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+
+        subsetsHelper(nums, {}, 0, res);
+
+        return res;
+    }
+};
