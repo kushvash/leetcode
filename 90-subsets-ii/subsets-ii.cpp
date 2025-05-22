9429560d04b49d8b7f80442b1357984d8ac18aba
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-21 18:31:14 -0700

    Time: 0 ms (100.00%) | Memory: 13.7 MB (19.02%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void subsetsWithDupHelper(vector<int>& nums, vector<int> currSet , int start, vector<vector<int>>& res) {
+    void subsetsWithDupHelper(vector<int>& nums, vector<int>& currSet , int start, vector<vector<int>>& res) {
         // base case
         if(start==nums.size()) {
             res.push_back(currSet);
@@ -9,18 +9,14 @@ public:
 
         // excluding current
         int i=start+1;
-
         while(i<nums.size() && nums[i]==nums[i-1]){
             i++;
         }
-
         subsetsWithDupHelper(nums, currSet, i, res);
 
         // including current
         currSet.push_back(nums[start]);
-
         subsetsWithDupHelper(nums, currSet, start+1, res);
-
         currSet.pop_back();
 
         return;
@@ -29,8 +25,9 @@ public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
+        vector<int> currSet;
 
-        subsetsWithDupHelper(nums, {}, 0, res);
+        subsetsWithDupHelper(nums, currSet, 0, res);
 
         return res;
     }
