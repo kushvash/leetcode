a78114cad781107259516d55919e950dd0a67b53
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-22 00:07:13 -0700

    Time: 75 ms (32.91%) | Memory: 110.1 MB (25.52%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -9,19 +9,19 @@ public:
             return;
         }
 
-        // // excluding currNum
-        // combineHelper(n, k, currNum+1, currSet, res);
-        // // including currNum
-        // currSet.push_back(currNum);
-        // combineHelper(n, k, currNum+1, currSet, res);
-        // currSet.pop_back();
+        // excluding currNum
+        combineHelper(n, k, currNum+1, currSet, res);
+        // including currNum
+        currSet.push_back(currNum);
+        combineHelper(n, k, currNum+1, currSet, res);
+        currSet.pop_back();
 
 
-        for(int i=currNum; i<=n; i++) {
-            currSet.push_back(i);
-            combineHelper(n, k, i+1, currSet, res);
-            currSet.pop_back();
-        }
+        // for(int i=currNum; i<=n; i++) {
+        //     currSet.push_back(i);
+        //     combineHelper(n, k, i+1, currSet, res);
+        //     currSet.pop_back();
+        // }
 
 
         return;
