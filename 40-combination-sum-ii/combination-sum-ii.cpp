c526d76d5ece125d39d9f0f321b5aed5cbcbebfd
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-21 18:08:30 -0700

    Time: 4 ms (26.64%) | Memory: 16.5 MB (13.57%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void combinationSumHelper(vector<int>& candidates, int start, vector<int> currSet, int target, vector<vector<int>>& res){
+    void combinationSumHelper(vector<int>& candidates, int start, vector<int>& currSet, int target, vector<vector<int>>& res){
         if(target==0){
             res.push_back(currSet);
             return;
@@ -9,26 +9,18 @@ public:
             return;
         }
 
-        // //excluding element
-        // int i=start;
-        // while(i<candidates.size() && (i==start || candidates[i]==candidates[i-1])){
-        //     i++;
-        // }
-        // combinationSumHelper(candidates, i, currSet, target, res);
+        //excluding element
+        int i=start;
+        while(i<candidates.size() && (i==start || candidates[i]==candidates[i-1])){
+            i++;
+        }
+        combinationSumHelper(candidates, i, currSet, target, res);
         
 
-        // //including element
-        // currSet.push_back(candidates[start]);
-        // combinationSumHelper(candidates, start+1, currSet, target-candidates[start], res);
-
-        for(int i=start; i<candidates.size(); i++){
-            if(i>0 && candidates[i]==candidates[i-1] && (i-1)>=start){
-                continue;
-            }
-            currSet.push_back(candidates[i]);
-            combinationSumHelper(candidates, i+1, currSet, target-candidates[i], res);
-            currSet.pop_back();
-        }
+        //including element
+        currSet.push_back(candidates[start]);
+        combinationSumHelper(candidates, start+1, currSet, target-candidates[start], res);
+        currSet.pop_back();
 
         return;
     }
@@ -36,7 +28,8 @@ public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         vector<vector<int>> res;
-        combinationSumHelper(candidates, 0, {}, target, res);
+        vector<int> currSet;
+        combinationSumHelper(candidates, 0, currSet, target, res);
 
         return res;
 
