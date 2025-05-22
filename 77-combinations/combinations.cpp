f8a924496992a167af5ae5a4aa9988b53f6c628c
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-21 23:59:07 -0700

    Time: 70 ms (42.77%) | Memory: 109.9 MB (28.18%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void combineHelper(int n, int k, int currNum, vector<int>& currSet, vector<vector<int>>& res) {
+        if(currSet.size()==k) {
+            res.push_back(currSet);
+            return;
+        }
+        if(currSet.size()>k || currNum>n) {
+            return;
+        }
+
+        // excluding currNum
+        combineHelper(n, k, currNum+1, currSet, res);
+        // including currNum
+        currSet.push_back(currNum);
+        combineHelper(n, k, currNum+1, currSet, res);
+        currSet.pop_back();
+
+
+        return;
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> currSet; 
+        vector<vector<int>> res;
+
+        combineHelper(n, k, 1, currSet, res);
+
+        return res;
+    }
+};
