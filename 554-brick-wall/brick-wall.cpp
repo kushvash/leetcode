2cef12d7da15a35b320081846a8ece958482f9a4
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-22 23:10:54 -0700

    Time: 22 ms (10.08%) | Memory: 28.3 MB (16.40%) - LeetSync

--- 554-brick-wall/brick-wall.cpp
+++ 554-brick-wall/brick-wall.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
         unordered_map<long long, int> record;
+        int maxEdges=0;
 
         for(vector<int> row: wall){
             long long curr=0;
             for(int i=0; i<row.size()-1; i++){
                 curr+=row[i];
                 record[curr]++;
-            }
-        }
-        int maxEdges=0;
 
-        for(auto i: record){
-            if(i.second>maxEdges){
-                maxEdges=i.second;
+                if(record[curr]>maxEdges){
+                    maxEdges=record[curr];
+                }
             }
         }
 
