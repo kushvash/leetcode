895be92f7d7fee0835d9cc9854cb0492d8256ed9
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-22 17:15:00 -0700

    Time: 23 ms (21.36%) | Memory: 45.8 MB (68.40%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -4,20 +4,14 @@ public:
         int count=0, l=0, r=people.size()-1;
         sort(people.begin(), people.end());
 
-        while(l<r) {
+        while(l<=r) {
             if(people[l]+people[r]<=limit){
-                count++;
                 l++;
-                r--;
-            }else{
-                count++;
-                r--;
-            }
+            }    
+            count++;
+            r--;
         }
 
-        if(l==r){
-            count++;
-        }
 
         return count;
     }
