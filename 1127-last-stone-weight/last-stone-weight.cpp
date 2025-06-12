b30ae96f1fe6c3053851002f0aed52fdef54f3e6
Kush <156246549+kushvash@users.noreply.github.com>
2025-06-11 17:42:30 -0700

    Time: 0 ms (100.00%) | Memory: 10.1 MB (9.44%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+        for (int s : stones) {
+            maxHeap.push(s);
+        }
+
+        while(maxHeap.size()>1){
+            int x=maxHeap.top();
+            maxHeap.pop();
+            int y=maxHeap.top();
+            maxHeap.pop();
+
+            if(x-y==0){
+                continue;
+            }
+
+            maxHeap.push(abs(x-y));
+        }
+
+        maxHeap.push(0);
+
+        return maxHeap.top();
+    }
+};
