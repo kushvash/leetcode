5de7cf14028db021ed143f79fbcc720f8c798b9d
Kush <156246549+kushvash@users.noreply.github.com>
2025-06-13 09:59:35 -0700

    Time: 0 ms (100.00%) | Memory: 44.1 MB (60.58%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
@@ -5,27 +5,27 @@ public:
         if(n==1){
             return 1;
         }
-        vector<int> incA(n);
-        vector<int> decA(n);
+        // vector<int> incA(n);
+        // vector<int> decA(n);
+        int inc=1, dec=1;
 
-        incA[0]=decA[0]=1;
         int res=1;
 
 
         for(int i=1; i<n; i++){
             if(arr[i]>arr[i-1]){
-                incA[i]=decA[i-1]+1;
-                decA[i]=1;
+                inc=dec+1;
+                dec=1;
             }
             else if(arr[i]<arr[i-1]){
-                decA[i]=incA[i-1]+1;
-                incA[i]=1;
+                dec=inc+1;
+                inc=1;
             }else{
-                decA[i]=1;
-                incA[i]=1;
+                dec=1;
+                inc=1;
             }
 
-            res=max(res, max(incA[i], decA[i]));
+            res=max(res, max(inc, dec));
         }
 
         return res;
