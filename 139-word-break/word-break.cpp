74cc589fe03404c8cd53e9d38f6889a64b6d401f
Kush <156246549+kushvash@users.noreply.github.com>
2025-06-14 22:20:21 -0700

    Time: 5 ms (59.04%) | Memory: 10.8 MB (79.42%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -7,13 +7,15 @@ public:
             record.insert(s);
         }
         int n=s.size();
+
         vector<bool> dp(n+1, false);
         dp[n]=true;
 
-
+        // checking if the i to j is in wordDict
         for(int i=n-1; i>=0; i--){
             string t="";
 
+            // checking if the j to n is in wordDict
             for(int j=i; j<n; j++){
                 t+=s[j];
 
