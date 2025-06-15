8fd7e4f3fe652c56290ee73b31c384ce695c9252
Kush <156246549+kushvash@users.noreply.github.com>
2025-06-14 19:24:53 -0700

    Time: 3 ms (45.98%) | Memory: 44 MB (98.82%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int maxTurbulenceSize(vector<int>& arr) {
-        int n=arr.size(), maxA=1;
-        vector<int> inc(n, 1), dec(n, 1);
+        int n=arr.size(), maxA=1, inc=1, dec=1;
+        if(n==1){
+            return 1;
+        }
 
         for(int i=1; i<n; i++){
             if(arr[i]>arr[i-1]){
-                inc[i]=dec[i-1]+1;
+                inc=dec+1;
+                dec=1;
             }else if(arr[i]<arr[i-1]){
-                dec[i]=inc[i-1]+1;
+                dec=inc+1;
+                inc=1;
+            }else {
+                inc=1;
+                dec=1;
             }
 
-            maxA=max(maxA, max(inc[i], dec[i]));
+            maxA=max(maxA, max(inc, dec));
         }
 
         return maxA;
