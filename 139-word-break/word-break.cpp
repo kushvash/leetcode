95cf78aef9dd3d539f6d16501622b72f3f5862cf
Kush <156246549+kushvash@users.noreply.github.com>
2025-06-19 00:59:24 -0700

    Time: 0 ms (100.00%) | Memory: 10.7 MB (79.12%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> record;
-
-        for(string s: wordDict){
-            record.insert(s);
-        }
         int n=s.size();
 
+        unordered_set<string> record;
+
         vector<bool> dp(n+1, false);
+
         dp[n]=true;
 
-        // checking if the i to j is in wordDict
+        for(string str: wordDict){
+            record.insert(str);
+        }
+
         for(int i=n-1; i>=0; i--){
-            string t="";
-
-            // checking if the j to n is in wordDict
+            string temp="";
             for(int j=i; j<n; j++){
-                t+=s[j];
+                temp+=s[j];
 
-                if(record.find(t)!=record.end() && dp[j+1]){
+                if(record.find(temp)!=record.end() && dp[j+1]){
                     dp[i]=true;
+
                     break;
                 }
             }
         }
 
+
         return dp[0];
+
     }
 };
