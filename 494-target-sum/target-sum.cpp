17e72209bc61147513d7120a918ea690461caa52
Kush <156246549+kushvash@users.noreply.github.com>
2025-06-20 19:07:40 -0700

    Time: 43 ms (43.45%) | Memory: 61.2 MB (10.48%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -3,22 +3,20 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
 
-        // for(int num:nums){
-        //     maxS+=num;
+        vector<vector<int>> dp(n+1, vector<int> (4005, 0));
+
+        dp[n][2000] = 1;
+
+        // for(int i=-1000; i<=1000; i++){        
+        //     if(i==nums[n-1]){
+        //         dp[n-1][i+2000]+=1;
+        //     }
+        //     if(i==-nums[n-1]){
+        //         dp[n-1][i+2000]+=1;
+        //     }
         // }
-
-        vector<vector<int>> dp(n, vector<int> (4005, 0));
-
-        for(int i=-1000; i<=1000; i++){        
-            if(i==nums[n-1]){
-                dp[n-1][i+2000]+=1;
-            }
-            if(i==-nums[n-1]){
-                dp[n-1][i+2000]+=1;
-            }
-        }
     
-        for(int i=n-2; i>=0; i--){
+        for(int i=n-1; i>=0; i--){
             for(int j=-1000; j<=1000; j++){
                 dp[i][j+2000]+=dp[i+1][j-nums[i]+2000];
                 dp[i][j+2000]+=dp[i+1][j+nums[i]+2000];
