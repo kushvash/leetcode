b1b5200bf6d4427bf26214756130419b141434ab
Kush <156246549+kushvash@users.noreply.github.com>
2025-07-03 13:35:56 -0700

    Time: 5 ms (49.54%) | Memory: 9.2 MB (76.58%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -10,10 +10,10 @@ public:
         vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
         
         dp[0][0] = true;
-        for (int i = 1; i <= s1.size(); ++i){
+        for (int i = 1; i <= s1.size(); i++){
             dp[i][0] = dp[i-1][0] && (s1[i-1] == s3[i-1]);
         }
-        for (int j = 1; j <= s2.size(); ++j){
+        for (int j = 1; j <= s2.size(); j++){
             dp[0][j] = dp[0][j-1] && (s2[j-1] == s3[j-1]);
         }
 
