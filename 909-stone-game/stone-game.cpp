b1d611e66b2da093ac8a6fbb2c67da3feb081a3c
Kush <156246549+kushvash@users.noreply.github.com>
2025-07-05 22:19:38 -0700

    Time: 0 ms (100.00%) | Memory: 10.3 MB (88.56%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int start=0, end=piles.size()-1, alice=0, bob=0;
+        return true;
+        // int start=0, end=piles.size()-1, alice=0, bob=0;
 
-        while(start<=end){
-            if(piles[start]>piles[end]){
-                alice+=piles[start];
-                start++;
-            }else{
-                alice+=piles[end];
-                end--;
-            }
-
-
-            if(start<end){
-                break;
-            }
-
-            if(piles[start]>piles[end]){
-                bob+=piles[start];
-                start++;
-            }else{
-                bob+=piles[end];
-                end--;
-            }
-        }
+        // while(start<=end){
+        //     if(piles[start]>piles[end]){
+        //         alice+=piles[start];
+        //         start++;
+        //     }else{
+        //         alice+=piles[end];
+        //         end--;
+        //     }
 
 
-        return alice>bob;
+        //     if(start<end){
+        //         break;
+        //     }
+
+        //     if(piles[start]>piles[end]){
+        //         bob+=piles[start];
+        //         start++;
+        //     }else{
+        //         bob+=piles[end];
+        //         end--;
+        //     }
+        // }
+
+
+        // return alice>bob;
     }
 };
