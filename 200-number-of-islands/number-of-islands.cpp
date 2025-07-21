4d74e2200c9eeae8bb77605807b66e44237c7822
Kush <156246549+kushvash@users.noreply.github.com>
2025-07-20 18:03:29 -0700

    Time: 19 ms (95.92%) | Memory: 16.6 MB (65.68%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void markIsland(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& record){
+        if(record[i][j]){
+            return;
+        }
+
+        record[i][j]=true;
+
+        if(i!=0 && grid[i-1][j]=='1'){
+            markIsland(i-1, j, grid, record);
+        }
+
+        if(j!=0 && grid[i][j-1]=='1'){
+            markIsland(i, j-1, grid, record);
+        } 
+
+        if(i!=grid.size()-1 && grid[i+1][j]=='1'){
+            markIsland(i+1, j, grid, record);
+        }
+
+        if(j!=grid[0].size()-1 && grid[i][j+1]=='1'){
+            markIsland(i, j+1, grid, record);    
+        } 
+
+        return;
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int m=grid.size(), n=grid[0].size(), count=0;
+
+        vector<vector<bool>> record(m, vector<bool> (n, false));
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]=='1' && !record[i][j]){
+                    markIsland(i, j, grid, record);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
