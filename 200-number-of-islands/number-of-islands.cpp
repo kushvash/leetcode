5e433069139b38c7938b14a070f1dc804581ce1c
Kush <156246549+kushvash@users.noreply.github.com>
2025-07-20 18:38:49 -0700

    Time: 25 ms (72.80%) | Memory: 16.1 MB (89.64%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,45 +1,32 @@
 class Solution {
 public:
-    void markIsland(int i, int j, int m, int n, vector<vector<char>>& grid, vector<vector<bool>>& record){
-        if(record[i][j]){
-            return;
-        }
+    int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-        record[i][j]=true;
-
-        if(i!=0 && grid[i-1][j]=='1'){
-            markIsland(i-1, j, m, n, grid, record);
-        }
-
-        if(j!=0 && grid[i][j-1]=='1'){
-            markIsland(i, j-1, m, n, grid, record);
-        } 
-
-        if(i!=m-1 && grid[i+1][j]=='1'){
-            markIsland(i+1, j, m, n, grid, record);
-        }
-
-        if(j!=n-1 && grid[i][j+1]=='1'){
-            markIsland(i, j+1, m, n, grid, record);    
-        } 
-
-        return;
-    }
-    
     int numIslands(vector<vector<char>>& grid) {
-        int m=grid.size(), n=grid[0].size(), count=0;
+        int ROWS = grid.size(), COLS = grid[0].size();
+        int islands = 0;
 
-        vector<vector<bool>> record(m, vector<bool> (n, false));
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j]=='1' && !record[i][j]){
-                    markIsland(i, j, m, n, grid, record);
-                    count++;
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (grid[r][c] == '1') {
+                    dfs(grid, r, c);
+                    islands++;
                 }
             }
         }
-
-        return count;
+        
+        return islands;
+    }
+    
+    void dfs(vector<vector<char>>& grid, int r, int c) {
+        if (r < 0 || c < 0 || r >= grid.size() || 
+            c >= grid[0].size() || grid[r][c] == '0') {
+            return;
+        }
+        
+        grid[r][c] = '0';
+        for (int i = 0; i < 4; i++) {
+            dfs(grid, r + directions[i][0], c + directions[i][1]);
+        }
     }
 };
