fac6661697b99777ad2a1b87e6ba58bd339bcd67
Kush <156246549+kushvash@users.noreply.github.com>
2025-07-22 18:30:35 -0700

    Time: 42 ms (13.70%) | Memory: 29 MB (9.10%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -22,25 +22,25 @@ public:
 
         q.push({r,c});
 
-        while (!q.empty()) {
-            int row = q.front()[0], col = q.front()[1];
+        while(!q.empty()){
+            int row=q.front()[0], col=q.front()[1];
             q.pop();
 
-            if (row > 0 && grid[row-1][col] == '1') {
+            if(row>0 && grid[row-1][col]=='1'){
                 q.push({row-1, col});
-                grid[row-1][col] = '0';
+                grid[row-1][col]='0';
             }
-            if (col > 0 && grid[row][col-1] == '1') {
+            if(col>0 && grid[row][col-1]=='1'){
                 q.push({row, col-1});
-                grid[row][col-1] = '0';
+                grid[row][col-1]='0';
             }
-            if (row < m-1 && grid[row+1][col] == '1') {
-                q.push({row+1, col});      // fixed
-                grid[row+1][col] = '0';
+            if(row<m-1 && grid[row+1][col]=='1'){
+                q.push({row+1, col});
+                grid[row+1][col]='0';
             }
-            if (col < n-1 && grid[row][col+1] == '1') {
-                q.push({row, col+1});      // fixed
-                grid[row][col+1] = '0';
+            if(col<n-1 && grid[row][col+1]=='1'){
+                q.push({row, col+1});
+                grid[row][col+1]='0';
             }
         }
     }
