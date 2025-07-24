545311e9337edeea22cad5ce0178946b68c20442
Kush <156246549+kushvash@users.noreply.github.com>
2025-07-23 17:45:40 -0700

    Time: 3 ms (68.29%) | Memory: 12.2 MB (31.05%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -21,32 +21,31 @@ public:
 
 class Solution {
 public:
+    Node* cloneGraphHelper(Node* root, unordered_map<Node*, Node*>& record){
+        if(!root){
+            return nullptr;    
+        }
+
+        if(record.find(root)!=record.end()){
+            return record[root];
+        }
+
+        record[root] = new Node(root->val);
+
+        for(Node* ne: root->neighbors){
+            record[root]->neighbors.push_back(cloneGraphHelper(ne, record));
+        }
+
+        return record[root];
+    }
+
     Node* cloneGraph(Node* node) {
         if(!node){
             return nullptr;
         }   
 
-        queue<Node*> q;
         unordered_map<Node*, Node*> record;
-
-        q.push(node);
-
-        record[node]=new Node(node->val);
-
-        while(!q.empty()){
-            Node* curr=q.front();
-            q.pop();
-
-            for(Node* ne: curr->neighbors){
-                if(record.find(ne)==record.end()){
-                    record[ne] = new Node(ne->val);
-                    q.push(ne); 
-                }
-
-                record[curr]->neighbors.push_back(record[ne]);
-            }
-        }
-
-        return record[node];
+        
+        return cloneGraphHelper(node, record);
     }
 };
