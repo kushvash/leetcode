4b03572255c669a3c1d4c307c99231fa87f24377
Kush <156246549+kushvash@users.noreply.github.com>
2025-08-17 13:44:11 -0700

    Time: 11 ms (58.24%) | Memory: 21.1 MB (38.80%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
+
+    int countSubstringsHelper(string s, int l, int r){
+        int count=0;
+
+        while(l>=0 && r<s.size() && s[l]==s[r]){
+            count++;
+            l--;
+            r++;
+        }
+
+        return count;
+    }
+
     int countSubstrings(string s) {
         int n=s.size(), count=0;
 
         for(int i=0; i<n; i++){
-            int l=i, r=i;
-
-            while(l>=0 && r<n && s[l]==s[r]){
-                count++;
-                l--;
-                r++;
-            }
-
-            l=i;
-            r=i+1;
-
-            while(l>=0 && r<n && s[l]==s[r]){
-                count++;
-                l--;
-                r++;
-            }
+            count+=countSubstringsHelper(s, i, i);
+            count+=countSubstringsHelper(s, i, i+1);
         }
 
         return count;
