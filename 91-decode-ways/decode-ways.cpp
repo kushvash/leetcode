ff2c74b4ab7a5f84802f7dff6ff9bc697ef2d5e7
Kush <156246549+kushvash@users.noreply.github.com>
2025-08-20 21:23:33 -0700

    Time: 2 ms (27.19%) | Memory: 8.7 MB (75.05%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -6,20 +6,26 @@ public:
             return 0;
         }
         
-        vector<int> dp(n + 1, 0);
-        dp[0] = 1;               // empty prefix
-        dp[1] = 1;               // first char is non-zero
+        vector<int> dp(n, 0);
+        dp[0] = 1;  // first char is non-zero
 
-        for (int i = 2; i <= n; ++i) {
-            // single digit s[i-1]
-            if (s[i-1] != '0')
-                dp[i] += dp[i-1];
+        for (int i = 1; i < n; ++i) {
+            int ways = 0;
 
-            // two digits s[i-2..i-1]
-            int two = (s[i-2] - '0') * 10 + (s[i-1] - '0');
-            if (two >= 10 && two <= 26)
-                dp[i] += dp[i-2];
+            // single digit s[i]
+            if (s[i] != '0') {
+                ways += dp[i-1];
+            }
+
+            // two digits s[i-1..i]
+            int two = (s[i-1]-'0')*10 + (s[i]-'0');
+            if (two >= 10 && two <= 26) {
+                ways += (i >= 2 ? dp[i-2] : 1); // empty prefix counts as 1
+            }
+
+            dp[i] = ways;
         }
-        return dp[n];
+
+        return dp[n-1];
     }
 };
