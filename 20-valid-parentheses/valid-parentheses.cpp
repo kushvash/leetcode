916774084d9bc1218e651a4bcf8c53e93e5de6cf
Kush <156246549+kushvash@users.noreply.github.com>
2025-08-21 13:32:15 -0700

    Time: 0 ms (100.00%) | Memory: 8.9 MB (19.85%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> helper;
-        unordered_map<char, char> dict={
-            {'}', '{'},
-            {']', '['},
-            {')', '('}
-        };
+        stack<char> st;
 
-        for(char c: s){
-            if(c=='{' || c=='[' || c=='('){
-                helper.push(c);
+        unordered_map<char, char> record;
+
+        record[')']='(';
+        record[']']='[';
+        record['}']='{';
+
+        for(char& c: s){
+            if(record.find(c)==record.end()){
+                st.push(c);
             }else{
-                if(helper.empty() || helper.top()!=dict[c]){
+                if(st.empty()){
                     return false;
+                }else if(st.top()!=record[c]){
+                    return false;
+                }else{
+                    st.pop();
                 }
-                helper.pop();
             }
         }
 
-        return helper.empty();
+        return st.empty();
+        
     }
 };
