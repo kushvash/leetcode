caf8bd21d975796f11c81ff768d0379fd819bca4
Kush <156246549+kushvash@users.noreply.github.com>
2025-08-27 12:57:51 -0700

    Time: 2 ms (66.40%) | Memory: 26.2 MB (5.37%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<int, unordered_set<int>> rows, columns;
+        int n=board.size();
+        unordered_map<int, unordered_set<int>> row, col;
         map<pair<int, int>, unordered_set<int>> block;
 
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
                 if(board[i][j]!='.'){
-                    pair<int, int> currPair = {i/3, j/3};
+                    pair<int, int> currBlock={i/3, j/3};
+                    int temp=board[i][j];
 
-                    if(rows[i].count(board[i][j]) || columns[j].count(board[i][j]) || block[currPair].count(board[i][j])){
+                    if(row[i].find(temp)!=row[i].end() || col[j].find(temp)!=col[j].end() || block[currBlock].find(temp)!=block[currBlock].end()){
                         return false;
                     }
 
-                    rows[i].insert(board[i][j]);
-                    columns[j].insert(board[i][j]);
-                    block[currPair].insert(board[i][j]);
+                    row[i].insert(temp);
+                    col[j].insert(temp);
+                    block[currBlock].insert(temp);
                 }
             }
         }
