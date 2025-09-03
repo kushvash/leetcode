9b6900188568d8dbef73a4c86dd1b80288445a4f
Kush <156246549+kushvash@users.noreply.github.com>
2025-09-03 13:42:31 -0700

    Time: 0 ms (100.00%) | Memory: 18.3 MB (5.63%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n=nums.size(), maxP=nums[0];
-                
-        if(n==1){
-            return nums[0];
-        }
-
-        vector<int> minDP(n, 0);
+        int n=nums.size(), maxP=nums[0];;
         vector<int> maxDP(n, 0);
-    
-        minDP[0]=maxDP[0]=nums[0];
+        vector<int> minDP(n, 0);
 
-        for(int i=1; i<n; i++){
-            maxDP[i]=max(nums[i], max(nums[i] * minDP[i-1], nums[i] * maxDP[i-1]));
-            minDP[i]=min(nums[i], min(nums[i] * minDP[i-1], nums[i] * maxDP[i-1]));
+        maxDP[0]=minDP[0]=nums[0];
+
+        for(int i=1; i<n; i++) {
+            maxDP[i]=max(nums[i], max(maxDP[i-1]*nums[i], minDP[i-1]*nums[i]));
+            minDP[i]=min(nums[i], min(maxDP[i-1]*nums[i], minDP[i-1]*nums[i]));
 
             maxP=max(maxP, maxDP[i]);
         }
 
         return maxP;
-        
     }
 };
