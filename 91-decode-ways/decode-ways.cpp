a116eb9c36fc2bdd41c5c2556076003c87ae8824
Kush <156246549+kushvash@users.noreply.github.com>
2025-09-05 16:08:21 -0700

    Time: 0 ms (100.00%) | Memory: 8.7 MB (61.92%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.size();
-        if (s[0] == '0') {
+        if (n == 0 || s[0] == '0') {
             return 0;
         }
         
         vector<int> dp(n, 0);
-        dp[0] = 1;  // first char is non-zero
+        dp[0] = 1;
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; ++i) {
             int ways = 0;
 
             // single digit s[i]
@@ -20,16 +20,15 @@ public:
             // two digits s[i-1..i]
             int two = (s[i-1]-'0')*10 + (s[i]-'0');
             if (two >= 10 && two <= 26) {
-                if(i>=2){
-                    ways+=dp[i-2];
-                }else{
-                    ways+=1;          // empty prefix counts as 1
+                if(i>=2) {
+                    ways+=dp[i-2];       
+                }else {
+                    ways+=1;
                 }
             }
 
             dp[i] = ways;
         }
-
         return dp[n-1];
     }
 };
