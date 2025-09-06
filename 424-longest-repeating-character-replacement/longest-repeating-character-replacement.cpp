df5ef02ed370adc9d0c24103f81fe51e2fe5ac5f
Kush <156246549+kushvash@users.noreply.github.com>
2025-09-06 14:33:57 -0700

    Time: 9 ms (33.51%) | Memory: 11.1 MB (18.76%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    int findMaxF(unordered_map<char, int>& record){
-        int maxF=0;
-        for(auto i: record){
-            maxF=max(maxF, i.second);
+    int maxCountIn(unordered_map<char, int>& record) {
+        int maxC=0;
+
+        for(auto& [c, i]:record) {
+            maxC=max(maxC, i);
         }
 
-        return maxF;
+        return maxC;
     }
+
     int characterReplacement(string s, int k) {
-        int l=0, r=0, maxL=0;
+        int left=0, right=0, maxCount=0, n=s.size();
         unordered_map<char, int> record;
 
-        while(r<s.size()){
-            record[s[r]]++;
-            while((r-l+1)-findMaxF(record)>k) {
-                record[s[l]]--;
-                l++;
+        while(right<n) {
+            record[s[right]]++;
+            while(((right-left+1)-maxCountIn(record))>k) {
+                record[s[left]]--;
+                left++;
             }
-            maxL=max(maxL, r-l+1);
-            r++;
+
+            maxCount=max(maxCount, right-left+1);
+            right++;
         }
 
-        return maxL;
+        return maxCount;
     }
 };
