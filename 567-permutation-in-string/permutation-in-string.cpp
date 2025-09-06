b9a912f1cdf6f70d80ded091d9b22626b2ec034b
Kush <156246549+kushvash@users.noreply.github.com>
2025-09-06 14:52:13 -0700

    Time: 0 ms (100.00%) | Memory: 10 MB (42.62%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,54 +1,44 @@
 class Solution {
 public:
+    bool checkMaps(vector<int>& rec1, vector<int>& rec2) {
+        for(int i=0; i<26; i++) {
+            if(rec1[i]!=rec2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
     bool checkInclusion(string s1, string s2) {
-        if(s1.size()>s2.size()){
+        int n1=s1.size(), n2=s2.size();
+
+        if(n1>n2) {
             return false;
         }
 
-        // unordered_map<char, int> m1, m2;
+        vector<int> rec1(26, 0);
+        vector<int> rec2(26, 0);
 
-        vector<int> m1(26,0), m2(26,0);
-
-        int matches=0, end=s1.size(), start=0;
-
-        for(int i=0; i<s1.size(); i++){
-            // m1[s1[i]]++;
-            // m2[s2[i]]++;
-
-            m1[s1[i]-'a']++;
-            m2[s2[i]-'a']++;
+        for(int i=0; i<n1; i++) {
+            rec1[s1[i]-'a']++;
+            rec2[s2[i]-'a']++;
         }
 
-        for(int i=0; i<26; i++){
-            if(m1[i]==m2[i]){
-                matches++;
-            }
-        }
+        int left=0, right=n1-1;
 
-        while(end<s2.size()){
-            if(matches==26){
+        while(right<n2) {
+            if(checkMaps(rec1, rec2)) {
                 return true;
             }
 
-            m2[s2[end]-'a']++;
-            if(m2[s2[end]-'a']==m1[s2[end]-'a']){
-                matches++;
-            }else if(m2[s2[end]-'a']==m1[s2[end]-'a']+1){
-                matches--;
+            rec2[s2[left]-'a']--;
+            left++;
+            right++;
+            if(right<n2) {
+                rec2[s2[right]-'a']++;
             }
-
-            m2[s2[start]-'a']--;
-            if(m2[s2[start]-'a']==m1[s2[start]-'a']){
-                matches++;
-            }else if(m2[s2[start]-'a']==m1[s2[start]-'a']-1){
-                matches--;
-            }
-
-            start++;
-            end++;
         }
 
-        return matches==26;
-
+        return false;
     }
 };
