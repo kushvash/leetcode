967049244e230712aff2c0915618a96283242cdb
Kush <156246549+kushvash@users.noreply.github.com>
2025-09-08 13:23:32 -0700

    Time: 39 ms (10.16%) | Memory: 111.9 MB (9.27%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -5,12 +5,15 @@ public:
         vector<int> res(n, 0);
         stack<pair<int, int>> st;
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++) {
             int t=temperatures[i];
-            while(!st.empty() && st.top().first<t){
-                res[st.top().second]=i-st.top().second;
+
+            while(!st.empty() && t>st.top().first) {
+                pair<int, int> temp=st.top();
                 st.pop();
+                res[temp.second]=i-temp.second;
             }
+
             st.push({t, i});
         }
 
