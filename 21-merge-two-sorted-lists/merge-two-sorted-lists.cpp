61348ea9acf1e3cc548dafc314c412e62bfc8dd5
Kush <156246549+kushvash@users.noreply.github.com>
2025-09-09 13:18:31 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (29.43%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,26 +10,27 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoListsHelper(ListNode* l1, ListNode* l2) {
-        // base cases
-        if (!l1) {
-            return l2;
-        } 
-        if (!l2) {
-            return l1;
-        } 
-        
-        // choose the smaller head, recurse on the rest
-        if (l1->val < l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *head=new ListNode(1);
+        ListNode *tempHead=head;
+        while(list1 && list2) {
+            if(list1->val>list2->val) {
+                tempHead->next=list2;
+                list2=list2->next;
+            }else {
+                tempHead->next=list1;
+                list1=list1->next;
+            }
+            tempHead=tempHead->next;
         }
-    }
 
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {        
-        return mergeTwoListsHelper(list1, list2);
+        if(list1) {
+            tempHead->next=list1;
+        }else if(list2) {
+            tempHead->next=list2;
+        }
+
+        return head->next;
+        
     }
 };
