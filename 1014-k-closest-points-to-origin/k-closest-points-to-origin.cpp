9c85e93d2b9504b0115ee1b3b34e57f03581e611
Kush <156246549+kushvash@users.noreply.github.com>
2025-09-14 19:14:51 -0700

    Time: 47 ms (55.95%) | Memory: 75.8 MB (43.07%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, pair<int, int>>> pq;
-
-        for(int i=0; i<points.size(); i++){
-            int dist=(points[i][0]*points[i][0])+(points[i][1]*points[i][1]);
-            pq.push({dist, {points[i][0], points[i][1]}});
-            if(pq.size()>k){
-                pq.pop();
-            }
+        priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> minHeap;
+    
+        for(vector<int> point: points) {
+            int dist=point[0]*point[0] + point[1]*point[1];
+            minHeap.push({dist, {point[0], point[1]}});
         }
-
+        
         vector<vector<int>> res;
-
-        while(!pq.empty()){
-            res.push_back({pq.top().second.first, pq.top().second.second});
-            pq.pop();
+        for(int i=0; i<k; i++) {
+            res.push_back({minHeap.top().second.first, minHeap.top().second.second});
+            minHeap.pop();
         }
 
         return res;
