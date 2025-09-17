d087823f973f4ee1e3c84aa424a8e5d7c03e5f22
Kush <156246549+kushvash@users.noreply.github.com>
2025-09-17 15:47:20 -0700

    Time: 0 ms (100.00%) | Memory: 10.4 MB (84.63%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int> curr;
-        vector<vector<int>> ans;
-
-        sort(nums.begin(), nums.end());
-        subsetsWithDupHelper(nums, ans, 0, curr);
-
-        return ans;
-
-    }
-
-    void subsetsWithDupHelper(vector<int>& nums, vector<vector<int>>& res, int i, vector<int>& curr){
-        if(i==nums.size()){
+    void subsetsWithDupHelper(vector<int>& nums, int i, vector<int>& curr, vector<vector<int>>& res) {
+        if(i==nums.size()) {
             res.push_back(curr);
             return;
         }
 
         // including nums[i]
         curr.push_back(nums[i]);
-        subsetsWithDupHelper(nums, res, i+1, curr);
+        subsetsWithDupHelper(nums, i+1, curr, res);
         curr.pop_back();
 
         // excluding nums[i]
-        i++;
-        while(i<nums.size() && nums[i]==nums[i-1]){
-            i++;
+        int j;
+        for(j=i+1; j<nums.size(); j++) {
+            if(nums[j]!=nums[j-1]) {
+                break;
+            }
         }
 
-        subsetsWithDupHelper(nums, res, i, curr);
+        subsetsWithDupHelper(nums, j, curr, res);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> curr;
+        vector<vector<int>> res;
 
-        return;
+        subsetsWithDupHelper(nums, 0, curr, res);
+
+        return res;
     }
 };
