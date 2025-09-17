e05068f55f69461f0d6a484bc4f4ee7839e6c7cc
Kush <156246549+kushvash@users.noreply.github.com>
2025-09-17 14:24:29 -0700

    Time: 7 ms (31.28%) | Memory: 17.9 MB (27.54%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    void combinationSumHelper(vector<int>& candidates, int target, int currentElement, int currSum, vector<int>& curr, vector<vector<int>>& res) {
-        if(currSum==target){
+    void combinationSumHelper(vector<int>& candidates, int target, int i, vector<int>& curr, int currSum, vector<vector<int>>& res) {
+        if(currSum==target) {
             res.push_back(curr);
             return;
-        }
-        if(currSum>target || currentElement==candidates.size()){
+        }if(currSum>target || i==candidates.size()) {
             return;
         }
 
-        // Excluding current element
-        combinationSumHelper(candidates, target, currentElement+1, currSum, curr, res);
+        // Not adding i
+        combinationSumHelper(candidates, target, i+1, curr, currSum, res);
 
-        // Including current element
-        curr.push_back(candidates[currentElement]);
-        currSum+=candidates[currentElement];
-        combinationSumHelper(candidates, target, currentElement, currSum, curr, res);
+        // Adding the i
+        curr.push_back(candidates[i]);
+        combinationSumHelper(candidates, target, i, curr, currSum+candidates[i], res);
         curr.pop_back();
     }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> curr;
         vector<vector<int>> res;
-        
-        combinationSumHelper(candidates, target, 0, 0, curr, res);
+
+        combinationSumHelper(candidates, target, 0, curr, 0, res);
 
         return res;
     }
