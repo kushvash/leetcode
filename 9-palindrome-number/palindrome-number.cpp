035e26a07e411e99357f7d5e30c12652ca4c877c
Kush <156246549+kushvash@users.noreply.github.com>
2025-09-18 13:50:16 -0700

    Time: 0 ms (100.00%) | Memory: 8.5 MB (91.75%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
-    bool isPalindromeS(vector<int> s) {
-        int n=s.size()-1;
-
-        for(int i=0; i<n; i++) {
-            if(s[i]!=s[n]) {
-                return false;
-            }
-            n--;
-        }
-
-        return true;
-    }
     bool isPalindrome(int x) {
-        if(x<0) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
 
-        vector<int> s;
-
-        while(x>0) {
-            s.push_back(x%10);
-            x/=10;
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
         }
 
-        return isPalindromeS(s); 
+        return x == revertedNumber || x == revertedNumber / 10;
     }
 };
