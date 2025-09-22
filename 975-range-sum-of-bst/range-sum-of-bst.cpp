5f3b3694598516f6b9a316cbce081337bc20c3cb
Kush <156246549+kushvash@users.noreply.github.com>
2025-09-21 23:05:12 -0700

    Time: 0 ms (100.00%) | Memory: 65.4 MB (9.93%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -13,15 +13,22 @@ class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         int sum = 0;
-        queue<TreeNode*> q; q.push(root);
+        queue<TreeNode*> q; 
+        q.push(root);
 
         while (!q.empty()) {
             TreeNode* node = q.front(); q.pop();
             if (!node) continue;
 
-            if (node->val >= low && node->val <= high) sum += node->val;
-            if (node->val > low)  q.push(node->left);   // left may contain valid values
-            if (node->val < high) q.push(node->right);  // right may contain valid values
+            if (node->val >= low && node->val <= high) {
+                sum += node->val;
+            }
+            if (node->val > low)  {
+                q.push(node->left);   // left may contain valid values
+            }
+            if (node->val < high) {
+                q.push(node->right);  // right may contain valid values
+            }
         }
         return sum;
     }
