6a85b66dc008b719c87196ecdcf6f0b5d5fc461e
Kush <156246549+kushvash@users.noreply.github.com>
2025-09-24 02:07:00 -0700

    Time: 0 ms (100.00%) | Memory: 11.9 MB (96.87%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/battleships-in-a-board.cpp
@@ -1,48 +1,14 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& board, int i, int j) {
-        queue<pair<int, int>> q;
-        vector<int> dirs;
-
-        if(i<board.size()-1 && board[i+1][j]=='X') {
-            dirs={1, 0};
-        }else if(j<board[0].size()-1 && board[i][j+1]=='X') {
-            dirs={0, 1};
-        }else {
-            board[i][j]='.';
-            // cout<<"single"<<endl;
-            return;
-        }
-        // cout<<dirs[0]<<" "<<dirs[1]<<endl;
-
-        q.push({i, j});
-
-        while(!q.empty()) {
-            auto tempP=q.front();
-            q.pop();
-            board[tempP.first][tempP.second]='.';
-
-            int newI=tempP.first+dirs[0], newJ=tempP.second+dirs[1];
-
-            if(newI<board.size() && newJ<board[0].size() && board[newI][newJ]=='X') {
-                q.push({newI, newJ});
-            }
-        }
-    }
-
-    int countBattleships(vector<vector<char>>& board) {
-        int m=board.size(), n=board[0].size(), count=0;
-
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(board[i][j]=='X') {
-                    cout<<i<<" "<<j<<endl;
-                    dfs(board, i, j);
-                    count++;
+    int countBattleships(vector<vector<char>>& b) {
+        int m = b.size(), n = b[0].size(), cnt = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (b[i][j] == 'X' && (i == 0 || b[i-1][j] != 'X') && (j == 0 || b[i][j-1] != 'X')) {
+                    cnt++;
                 }
             }
         }
-
-        return count;
+        return cnt;
     }
 };
