1cbc8e611be1436d981be87be572bdf6b609ff47
Kush <156246549+kushvash@users.noreply.github.com>
2025-10-05 17:03:39 -0700

    Time: 0 ms (100.00%) | Memory: 13.8 MB (75.66%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> dfs(int u, int target, const vector<vector<int>>& g, vector<vector<vector<int>>>& memo) {
-        if (!memo[u].empty()) return memo[u];
-        if (u == target) return memo[u] = {{target}};
-
-        vector<vector<int>> res;
-        for (int v : g[u]) {
-            auto tails = dfs(v, target, g, memo);
-            for (auto& path : tails) {
-                vector<int> p; p.reserve(path.size() + 1);
-                p.push_back(u);
-                p.insert(p.end(), path.begin(), path.end());
-                res.push_back(std::move(p));
-            }
+    void dfs(int u, int target, const vector<vector<int>>& g, vector<int>& path, vector<vector<int>>& res) {
+        if (u == target) { 
+            res.push_back(path); 
+            return; 
         }
-        return memo[u] = std::move(res);
+    
+        for (int v : g[u]) {
+            path.push_back(v);
+            dfs(v, target, g, path, res);
+            path.pop_back();
+        }
     }
     
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        int target = graph.size() - 1;
-        vector<vector<vector<int>>> memo(graph.size());
-        vector<int> cur; cur.push_back(0);
-        return dfs(0, target, graph, memo);
+        vector<int> curr;
+        curr.push_back(0);
+        vector<vector<int>> res;
+
+        int n=graph.size();
+
+
+
+        dfs(0, graph.size()-1, graph, curr, res);
+    
+        return res;
     }
 };
