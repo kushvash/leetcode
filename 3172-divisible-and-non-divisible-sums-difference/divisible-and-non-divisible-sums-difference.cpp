95b5a032d24e25615911d06b086ee4d9bb65dfd0
Kush <156246549+kushvash@users.noreply.github.com>
2025-10-24 23:47:35 -0700

    Time: 1 ms (9.67%) | Memory: 8.3 MB (67.20%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int div=0, notDiv=0;
-
-        for(int i=1; i<=n; i++) {
-            if(i%m==0) {
-                div+=i;
-            }else {
-                notDiv+=i;
-            }
-        }
-
-        return notDiv-div;
+        int k = n / m;
+        return n * (n + 1) / 2 - k * (k + 1) * m;
     }
 };
