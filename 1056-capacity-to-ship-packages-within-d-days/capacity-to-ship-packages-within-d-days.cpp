739c607a791ece372021b9e74c39fce7f7bdc2e5
Kush <156246549+kushvash@users.noreply.github.com>
2025-10-28 15:16:13 -0700

    Time: 12 ms (52.56%) | Memory: 34.9 MB (85.49%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool ifPossible(vector<int>& weights, int days, int cap) {
+        int currDay=1, currW=0;
+
+        for(int w: weights) {
+            if (w > cap) {
+                return false;
+            }
+
+            if (currW + w > cap) {
+                currDay++;
+                currW = w;
+            } else {
+                currW += w;
+            }
+
+            if (currDay > days) {
+                return false;
+            }
+        }
+
+        return currDay<=days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int lo=1, mid, hi=0;
+
+        for(int w: weights) {
+            lo=max(lo, w);
+            hi+=w;
+        }
+
+        while(lo<hi) {
+            mid=lo+(hi-lo)/2;
+
+            if(ifPossible(weights, days, mid)) {
+                hi=mid;
+            }else {
+                lo=mid+1;
+            }
+        }
+
+        return lo;
+    }
+};
