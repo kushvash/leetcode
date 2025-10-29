ee3021aeae1b850bf77edff1d918e03e58ee957f
Kush <156246549+kushvash@users.noreply.github.com>
2025-10-28 23:48:16 -0700

    Time: 1 ms (11.58%) | Memory: 8.7 MB (30.03%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if (needle.empty()) return 0;
-
+        if (needle.empty()) {
+            return 0;
+        }
+        
         int n = haystack.size(), m = needle.size();
         vector<int> lps(m, 0);
 
         // --- Build LPS array ---
         for (int i = 1, len = 0; i < m; ) {
             if (needle[i] == needle[len]) {
-                lps[i++] = ++len;
+                len++;
+                lps[i] = len;
+                i++;
             } else if (len != 0) {
                 len = lps[len - 1];
             } else {
-                lps[i++] = 0;
+                lps[i] = 0;
+                i++;
             }
         }
 
         // --- Search ---
         for (int i = 0, j = 0; i < n; ) {
             if (haystack[i] == needle[j]) {
-                i++; j++;
+                i++; 
+                j++;
                 if (j == m) return i - j;  // full match
             } else if (j != 0) {
                 j = lps[j - 1];
