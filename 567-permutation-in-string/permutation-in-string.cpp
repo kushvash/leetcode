1b4c7148aeea93505b918952ef5d7a578d8095de
Kush <156246549+kushvash@users.noreply.github.com>
2025-11-08 17:31:45 -0800

    Time: 0 ms (100.00%) | Memory: 10.1 MB (43.72%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,56 +1,64 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.length() > s2.length()) {
+        int n1=s1.size(), n2=s2.size(), totalMatched=0, l=0, r=0;
+
+        if(n1>n2) {
             return false;
         }
 
-        vector<int> s1Count(26, 0);
-        vector<int> s2Count(26, 0);
-       
-        for(int i=0; i<s1.size(); i++) {
-            s1Count[s1[i]-'a']++;
-            s2Count[s2[i]-'a']++;
+        vector<int> countS1(26, 0);
+        vector<int> countS2(26, 0);
+
+        for(char c: s1) {
+            countS1[c-'a']++;
         }
 
-        int matches=0;
-
-        for(int i=0; i<26; i++) {
-            if(s1Count[i]==s2Count[i]) {
-                matches++;
+        for (int i = 0; i < 26; i++) {
+            if (countS1[i] == countS2[i]) {
+                totalMatched++;
             }
         }
 
-        int l=0, r=s1.size();
-
-        while(r<s2.size()) {
-            if(matches==26) {
-                return true;
+        for(int i=0; i<n1; i++) {
+            char c=s2[i];
+            countS2[c-'a']++;
+            if(countS2[c-'a']-countS1[c-'a']==0) {
+                totalMatched++;
+            }else if(countS2[c-'a']-countS1[c-'a']==1) {
+                totalMatched--;
             }
-
-            int index=s2[l]-'a';
-            s2Count[index]--;
-
-            if(s1Count[index]==s2Count[index]) {
-                matches++;
-            }else if(s1Count[index]==s2Count[index]+1) {
-                matches--;
-            }
-
-
-            index=s2[r]-'a';
-            s2Count[index]++;
-
-            if(s1Count[index]==s2Count[index]) {
-                matches++;
-            }else if(s1Count[index]==s2Count[index]-1) {
-                matches--;
-            }
-
-            l++;
             r++;
         }
 
-        return matches==26;
+        if(totalMatched==26) {
+            return true;
+        }
+
+        while(r<n2) {
+            char del=s2[l], ins=s2[r];
+
+            countS2[ins-'a']++;
+            if(countS2[ins-'a']-countS1[ins-'a']==0) {
+                totalMatched++;
+            }else if(countS2[ins-'a']-countS1[ins-'a']==1) {
+                totalMatched--;
+            }
+            r++;
+            
+            countS2[del-'a']--;
+            if(countS2[del-'a']-countS1[del-'a']==0) {
+                totalMatched++;
+            }else if(countS2[del-'a']-countS1[del-'a']==-1) {
+                totalMatched--;
+            }
+            l++;
+
+            if(totalMatched==26) {
+                return true;
+            }
+        }
+
+        return false;
     }
 };
