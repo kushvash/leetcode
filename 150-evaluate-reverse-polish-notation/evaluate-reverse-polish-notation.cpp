901ab19ed19cab2963f7045d0b1b497de44ec038
Kush <156246549+kushvash@users.noreply.github.com>
2025-11-09 14:20:31 -0800

    Time: 4 ms (21.54%) | Memory: 17.1 MB (36.02%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        unordered_set<string> record={
-            "+", "-", "/", "*"
-        };
         stack<int> st;
 
-        for(string& token: tokens) {
-            if(record.find(token)==record.end()) {
-                st.push(stoi(token));
-            }else {
-                int num1=st.top();
+        for(string s: tokens) {
+            if(s=="+" || s=="-" || s=="*" || s=="/") {
+                int n1, n2;
+                n1=st.top();
                 st.pop();
-                int num2=st.top();
+                n2=st.top();
                 st.pop();
 
-                if(token=="+") {
-                    st.push(num1+num2);
-                }else if(token=="-") {
-                    st.push(num2-num1);
-                }else if(token=="/") {
-                    st.push(num2/num1);
+                int next;
+
+                if(s=="+") {
+                    next=n1+n2;
+                }else if(s=="-") {
+                    next=n2-n1;
+                }else if(s=="*") {
+                    next=n1*n2;
                 }else {
-                    st.push(num1*num2);
+                    next=n2/n1;
                 }
+
+                st.push(next);
+            }else {
+                st.push(stoi(s));
             }
         }
-
         return st.top();
     }
 };
