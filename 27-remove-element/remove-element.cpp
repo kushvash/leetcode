4bcedd3dc5c6660d1bfdb674a3bc59911cc66a88
Kush <156246549+kushvash@users.noreply.github.com>
2025-11-20 02:30:41 -0800

    Time: 0 ms (100.00%) | Memory: 11.7 MB (81.53%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -4,7 +4,8 @@ public:
         int k = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != val) {
-                nums[k++] = nums[i];
+                nums[k] = nums[i];
+                k++;
             }
         }
         return k;
