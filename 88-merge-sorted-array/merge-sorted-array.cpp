16b3655787a9cd3f01b3f576aa0d6fdf983cdd9a
Kush <156246549+kushvash@users.noreply.github.com>
2025-11-20 02:15:33 -0800

    Time: 0 ms (100.00%) | Memory: 12.3 MB (70.58%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -3,23 +3,25 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         m--;
         n--;
-        int i=nums1.size()-1;
+
+        int pointer=nums1.size()-1;
 
         while(m>=0 && n>=0) {
-            if(nums1[m]>nums2[n]) {
-                nums1[i]=nums1[m];
+            if(nums1[m]>=nums2[n]) {
+                nums1[pointer]=nums1[m];
                 m--;
             }else {
-                nums1[i]=nums2[n];
+                nums1[pointer]=nums2[n];
                 n--;
             }
 
-            i--;
+            pointer--;
         }
 
         while(n>=0) {
-            nums1[n]=nums2[n];
+            nums1[pointer]=nums2[n];
             n--;
+            pointer--;
         }
 
         return;
