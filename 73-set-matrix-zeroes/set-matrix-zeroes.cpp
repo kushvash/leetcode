e28d6aa3083304cfed77aec2ef8a1ea8683e4585
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-01 20:09:25 -0800

    Time: 0 ms (100.00%) | Memory: 18.5 MB (78.40%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size(), n=matrix[0].size();
-        unordered_set<int> row;
-        unordered_set<int> col;
+        int ROWS=matrix.size(), COLS=matrix[0].size();
+        bool isCol=false;
 
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(matrix[i][j]==0) {
-                    row.insert(i);
-                    col.insert(j);
+        for(int row=0; row<ROWS; row++) {
+            if(matrix[row][0]==0) {
+                isCol=true;
+            }
+            for(int col=1; col<COLS; col++) {
+                if(matrix[row][col]==0) {
+                    matrix[row][0]=0;
+                    matrix[0][col]=0;
                 }
             }
         }
 
-        for(int i: row) {
-            for(int j=0; j<n; j++) {
-                matrix[i][j]=0;
+        for(int row=1; row<ROWS; row++) {
+            for(int col=1; col<COLS; col++) {
+                if(matrix[row][0]==0 || matrix[0][col]==0) {
+                    matrix[row][col]=0;
+                }
             }
         }
-        for(int j: col) {
-            for(int i=0; i<m; i++) {
-                matrix[i][j]=0;
+
+        if(matrix[0][0]==0) {
+            for(int col=0; col<COLS; col++) {
+                matrix[0][col]=0;
+            }
+        }
+
+        if(isCol) {
+            for(int row=0; row<ROWS; row++) {
+                matrix[row][0]=0;
             }
         }
     }
