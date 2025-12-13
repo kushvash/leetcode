a5639caf9481d955a4d6fedf0e135dd824176e33
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-13 15:45:13 -0800

    Time: 11 ms (30.26%) | Memory: 71.3 MB (13.16%) - LeetSync

---
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool validCode(string s) {
+        string res;
+
+        for(char& c: s) {
+            if(!isalnum(c) && c!='_') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        int n=code.size();
+        vector<string> res;
+
+        unordered_map<string, vector<string>> mp;
+
+        for(int i=0; i<n; i++) {
+            if(!isActive[i] || code[i]=="") {
+                continue;
+            }
+
+            string business=businessLine[i];
+            string couponCode=code[i];
+
+            if(validCode(couponCode)) {
+                mp[business].push_back(couponCode);
+            }
+        }
+
+        vector<string> order = {"electronics", "grocery", "pharmacy", "restaurant"};
+
+        for(string& business: order) {
+            vector<string> code=mp[business];
+            sort(code.begin(), code.end());
+
+            for(string& c: code) {
+                res.push_back(c);
+            }
+        }
+
+        return res;
+    }
+};
