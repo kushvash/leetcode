9a92188a116010e003647ded01d4df3f4895cee0
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-13 15:15:09 -0800

    Time: 28 ms (71.77%) | Memory: 25.4 MB (75.27%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.cpp
+++ 271-encode-and-decode-strings/encode-and-decode-strings.cpp
@@ -1,16 +1,40 @@
 class Codec {
 public:
-    vector<string> ans;
     // Encodes a list of strings to a single string.
     string encode(vector<string>& strs) {
-        this->ans=strs;
+        string res;
+        for(string& str: strs) {
+            int sz=str.size();
 
-        return "";
+            res+=to_string(sz);
+            res+='#';
+            res+=str;
+        }
+
+        return res;
     }
 
     // Decodes a single string to a list of strings.
     vector<string> decode(string s) {
-        return ans;
+        vector<string> res;
+        int i=0;
+
+        while(i<s.size()) {
+            int j=i;
+            while(s[j]!='#') {
+                j++;
+            }
+
+            int sz=stoi(s.substr(i, j-i+1));
+
+            i=j+1;
+
+            res.push_back(s.substr(i, sz));
+
+            i+=sz;
+        }
+
+        return res;
     }
 };
 
