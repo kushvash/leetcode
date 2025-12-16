e48d1c148c0ce33e07d12a89d4e37295811e0ef9
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-16 08:43:19 -0800

    Time: 3 ms (64.48%) | Memory: 18.1 MB (40.94%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string, int> freq;
+        unordered_map<string, int> mp;
+
+        for(string& word: words) {
+            mp[word]++;
+        }
 
         auto cmp=[](pair<int, string>& a, pair<int, string>& b) {
             if(a.first==b.first) {
-                return a.second>b.second;
+                return a.second<b.second;
             }
-            return a.first<b.first;
+
+            return a.first>b.first;
         };
 
-        priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(cmp)> pq(cmp);
+        priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(cmp)> minHeap(cmp);
+
+        for(auto& [word, freq]: mp) {
+            minHeap.push({freq, word});
+
+            if(minHeap.size()>k) {
+                minHeap.pop();
+            }
+        }
 
         vector<string> res;
 
-        for(string& word: words) {
-            freq[word]++;
+        while(!minHeap.empty()) {
+            res.push_back(minHeap.top().second);
+            minHeap.pop();
         }
 
-
-        for(auto& [word, f]: freq) {
-            pq.push({f, word});
-        }
-        
-        for(int i=0; i<k; i++) {
-            auto [f, word]=pq.top();
-            pq.pop();
-
-            res.push_back(word);
-        }
+        reverse(res.begin(), res.end());
 
         return res;
     }
