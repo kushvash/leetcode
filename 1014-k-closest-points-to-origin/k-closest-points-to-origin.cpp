66262c87ece6733a74f20399d5a4c65c23fe29bd
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-16 07:58:37 -0800

    Time: 77 ms (38.00%) | Memory: 76.3 MB (39.31%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -5,12 +5,21 @@ public:
             return((a[0]*a[0])+(a[1]*a[1])<(b[0]*b[0])+(b[1]*b[1]));
         };
 
-        sort(points.begin(), points.end(), cmp);
+        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> maxHeap(cmp);
+
+        for(vector<int>& point: points) {
+            maxHeap.push(point);
+
+            if(maxHeap.size()>k) {
+                maxHeap.pop();
+            }
+        }
 
         vector<vector<int>> res;
 
-        for(int i=0; i<k; i++) {
-            res.push_back(points[i]);
+        while(!maxHeap.empty()) {
+            res.push_back(maxHeap.top());
+            maxHeap.pop();
         }
 
         return res;
