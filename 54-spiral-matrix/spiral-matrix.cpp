5076cce488ea6bc7fa8b90d79f3a5141c4db529d
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-17 15:11:28 -0800

    Time: 0 ms (100.00%) | Memory: 9.2 MB (79.35%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,42 +1,61 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        if (matrix.empty()) {
-            return {};
-        }
-        
-        int top = 0, bottom = matrix.size() - 1;
-        int left = 0, right = matrix[0].size() - 1;
+        int left=0, right=matrix[0].size()-1, top=0, bottom=matrix.size()-1, count=0, m=matrix.size(), n=matrix[0].size();
         vector<int> res;
 
-        while (top <= bottom && left <= right) {
-            // Traverse right along top row
-            for (int i = left; i <= right; i++) {
+        while(count!=m*n) {
+            // top row (all elements)
+            for(int i=left; i<=right; i++) {
                 res.push_back(matrix[top][i]);
+                count++;
             }
+
+            if(count==m*n) {
+                break;
+            }
+            
             top++;
 
-            // Traverse down along right column
-            for (int j = top; j <= bottom; j++) {
-                res.push_back(matrix[j][right]);
+            // right column (all but first element)
+            for(int i=top; i<=bottom; i++) {
+                res.push_back(matrix[i][right]);
+                count++;
             }
+
+            if(count==m*n) {
+                break;
+            }
+            
             right--;
 
-            // Traverse left along bottom row (if exists)
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    res.push_back(matrix[bottom][i]);
-                }
-                bottom--;
+            // if(top>=bottom || left>=right) {
+            //     break;
+            // }
+
+            // bottom row (all but last element)
+            for(int i=right; i>=left; i--) {
+                res.push_back(matrix[bottom][i]);
+                count++;
             }
 
-            // Traverse up along left column (if exists)
-            if (left <= right) {
-                for (int j = bottom; j >= top; j--) {
-                    res.push_back(matrix[j][left]);
-                }
-                left++;
+            if(count==m*n) {
+                break;
             }
+            
+            bottom--;
+
+            // left column (all but last element)
+            for(int i=bottom; i>=top; i--) {
+                res.push_back(matrix[i][left]);
+                count++;
+            }
+
+            if(count==m*n) {
+                break;
+            }
+
+            left++;
         }
 
         return res;
