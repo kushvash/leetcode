93f3137a11d46648033faceee14e7028489c4560
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-17 15:30:06 -0800

    Time: 0 ms (100.00%) | Memory: 9.3 MB (50.09%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -4,15 +4,10 @@ public:
         int left=0, right=matrix[0].size()-1, top=0, bottom=matrix.size()-1, count=0, m=matrix.size(), n=matrix[0].size();
         vector<int> res;
 
-        while(count!=m*n) {
+        while(top<=bottom && left<=right) {
             // top row (all elements)
             for(int i=left; i<=right; i++) {
                 res.push_back(matrix[top][i]);
-                count++;
-            }
-
-            if(count==m*n) {
-                break;
             }
             
             top++;
@@ -20,39 +15,28 @@ public:
             // right column (all but first element)
             for(int i=top; i<=bottom; i++) {
                 res.push_back(matrix[i][right]);
-                count++;
-            }
-
-            if(count==m*n) {
-                break;
             }
             
             right--;
 
-            // if(top>=bottom || left>=right) {
-            //     break;
-            // }
+            if(top>bottom) {
+                break;
+            }
 
             // bottom row (all but last element)
             for(int i=right; i>=left; i--) {
                 res.push_back(matrix[bottom][i]);
-                count++;
             }
+         
+            bottom--;
 
-            if(count==m*n) {
+            if(left>right) {
                 break;
             }
-            
-            bottom--;
 
             // left column (all but last element)
             for(int i=bottom; i>=top; i--) {
                 res.push_back(matrix[i][left]);
-                count++;
-            }
-
-            if(count==m*n) {
-                break;
             }
 
             left++;
