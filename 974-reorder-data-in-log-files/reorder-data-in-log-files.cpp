5dc5aecb52c56ea5a5275787d5e7637e128cdc4e
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-17 15:51:42 -0800

    Time: 4 ms (57.76%) | Memory: 14.2 MB (91.10%) - LeetSync

--- 974-reorder-data-in-log-files/reorder-data-in-log-files.cpp
+++ 974-reorder-data-in-log-files/reorder-data-in-log-files.cpp
@@ -16,14 +16,16 @@ public:
 
             string identifier=s.substr(0, identifierEnd);
 
+            string logs = s.substr(identifierEnd+1, s.size()-(identifierEnd+1));
+
             if(s[identifierEnd+1]>='0' && s[identifierEnd+1]<='9') {
-                intLogs.push_back({identifier, s.substr(identifierEnd+1, s.size()-identifierEnd)});
+                intLogs.push_back({identifier, logs});
             }else {
-                letterLogs.push_back({identifier, s.substr(identifierEnd+1, s.size()-identifierEnd)});
+                letterLogs.push_back({identifier, logs});
             }
         }
 
-        auto cmp=[](pair<string, string>& a, pair<string, string>& b) {
+        auto cmp=[](const pair<string, string>& a, const pair<string, string>& b) {
             if(a.second==b.second) {
                 return a.first<b.first;
             }
