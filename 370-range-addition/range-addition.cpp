2c2439c8fdfad300a61ad749799e77c07f993af9
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-18 07:54:29 -0800

    Time: 7 ms (21.27%) | Memory: 21 MB (74.69%) - LeetSync

--- 370-range-addition/range-addition.cpp
+++ 370-range-addition/range-addition.cpp
@@ -3,15 +3,16 @@ public:
     vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
         vector<int> res(length, 0);
 
-        for(auto& update: updates) {
-            int inc=update[2];
-            res[update[0]]+=inc;
-            if(update[1]+1<length) {
-                res[update[1]+1]-=inc;
+        // creating a diff array
+        for (auto &u : updates) {
+            int left = u[0], right = u[1], inc = u[2];
+            res[left] += inc;
+            if (right + 1 < length) {
+                res[right + 1] -= inc;
             }
         }
 
-        for(int i=0; i<length-1; i++) {
+        for (int i=0; i<length-1; i++) {
             res[i+1]+=res[i];
         }
 
