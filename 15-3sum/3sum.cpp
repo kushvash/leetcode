524d5d50b819f30829a9b0c4ff053f8454e06a67
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-19 11:57:18 -0800

    Time: 47 ms (67.51%) | Memory: 29.1 MB (46.01%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
+        int n=nums.size(), target=0;
+
         sort(nums.begin(), nums.end());
 
         vector<vector<int>> res;
 
-        for(int l=0; l<n-2; l++) {
-            if(l>0 && nums[l]==nums[l-1]) {
+        for(int i=0; i<n; i++) {
+            if(i>0 && nums[i]==nums[i-1]) {
                 continue;
             }
 
-            int mid=l+1, r=n-1;
-            
-            while(mid<r) {
-                if(r<n-1 && nums[r]==nums[r+1]) {
-                    r--;
-                    continue;
-                }if(mid>l+1 && nums[mid]==nums[mid-1]) {
-                    mid++;
-                    continue;
-                } 
+            if(nums[i]>target) {
+                break;
+            }
 
-                if(nums[l]+nums[mid]+nums[r]==0) {
-                    res.push_back({nums[l], nums[mid], nums[r]});
-                    mid++;
-                    r--;
-                }else if(nums[l]+nums[mid]+nums[r]>0) {
-                    r--;
+            int left=i+1;
+            int right=n-1;
+
+            while(left<right) {
+                if(left>i+1 && nums[left]==nums[left-1]) {
+                    left++;
+                    continue;
+                }
+
+                int sum=nums[i]+nums[left]+nums[right];
+
+                if(sum==target) {
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                }else if(sum>target) {
+                    right--;
                 }else {
-                    mid++;
+                    left++;
                 }
             }
         }
