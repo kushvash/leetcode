401138bf60fa924986588c453fe4f15efe75483a
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-21 15:06:21 -0800

    Time: 15 ms (36.80%) | Memory: 13.7 MB (99.77%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -3,7 +3,8 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
         
-        int n=nums.size(), res=nums[0]+nums[1]+nums[2];
+        int n=nums.size();
+        long long res=nums[0]+nums[1]+nums[2];
 
         for(int i=0; i<n-2; i++) {
             int left=i+1, right=n-1;
@@ -12,14 +13,11 @@ public:
                 long long currSum=nums[i]+nums[left]+nums[right];
                 if(abs(target-res)>abs(target-currSum)) {
                     res=currSum;
-                    if(res==target) {
-                        return res;
-                    }else if(currSum>target) {
-                        right--;
-                    }else {
-                        left++;
-                    }
-                }else if(currSum>target) {
+                }
+                if(res==target) {
+                    return res;
+                }
+                if(currSum>target) {
                     right--;
                 }else {
                     left++;
