4fc5717a1b08f887300143b0a0418bf0ecab2e7e
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-20 18:51:05 -0800

    Time: 3 ms (56.20%) | Memory: 16.1 MB (81.65%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -12,7 +12,7 @@ public:
         priority_queue<int, vector<int>, greater<int>> minHeap;
 
         for(int i=0; i<n; i++) {
-            while(!minHeap.empty() && minHeap.top()<=intervals[i][0]) {
+            if(!minHeap.empty() && minHeap.top()<=intervals[i][0]) {
                 minHeap.pop();
             }
 
