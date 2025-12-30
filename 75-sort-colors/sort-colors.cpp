69955cc7c8fc4963e7e0b2ea7643b8a82f13a3e2
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-30 15:02:17 -0800

    Time: 0 ms (100.00%) | Memory: 11.6 MB (47.50%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -6,16 +6,18 @@ public:
         while(mid<=right) {
             if(nums[mid]==0) {
                 swap(nums[left], nums[mid]);
-                left++;
                 mid++;
+                left++;
             }else if(nums[mid]==2) {
                 swap(nums[mid], nums[right]);
                 right--;
             }else {
                 mid++;
             }
-        }
+        } 
 
         return;
     }
 };
+
+// [0, 0, 2, 1, 1, 2]
