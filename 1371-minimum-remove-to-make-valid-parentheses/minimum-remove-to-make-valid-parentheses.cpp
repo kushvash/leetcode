40d42e2e7e6d3a3fb20548209d095851d4f8b8e2
Kush <156246549+kushvash@users.noreply.github.com>
2025-12-30 21:04:49 -0800

    Time: 14 ms (24.72%) | Memory: 14.6 MB (37.43%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        int cnt=0;
+        stack<char> st;
+
+        for(char c: s) {
+            if(c=='(') {
+                st.push(c);
+                cnt++;
+            }else if(c==')' && cnt>0) {
+                st.push(c);
+                cnt--;
+            }else if(c!=')') {
+                st.push(c);
+            }
+        }   
+
+        string res;
+
+        while(!st.empty()) {
+            char c=st.top();
+            st.pop();
+
+            if(c=='(' && cnt>0) {
+                cnt--;
+            }else {
+                res+=c;
+            }
+        }
+
+        reverse(res.begin(), res.end());
+
+        return res;
+    }
+};
