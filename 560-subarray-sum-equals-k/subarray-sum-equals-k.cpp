bccd9e644c993d2635b9dda51a3e9a4054000cb8
Kush <156246549+kushvash@users.noreply.github.com>
2026-01-02 07:41:22 -0800

    Time: 38 ms (76.34%) | Memory: 45.4 MB (55.35%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,16 +4,14 @@ public:
         int n=nums.size(), res=0, sum=0;
 
         unordered_map<int, int> mp;
-
+        
         mp[0]=1;
 
-        for(int i=0; i<n; i++) {
-            sum+=nums[i];
+        for(int num: nums) {
+            sum+=num;
 
-            int diff=sum-k;
-
-            if(mp.find(diff)!=mp.end()) {
-                res+=mp[diff];
+            if(mp.find(sum-k)!=mp.end()) {
+                res+=mp[sum-k];
             }
 
             mp[sum]++;
