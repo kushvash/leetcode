7e0064101cd2d72c457c9c1ba74bbce28de52232
Kush <156246549+kushvash@users.noreply.github.com>
2026-01-02 19:32:24 -0800

    Time: 0 ms (100.00%) | Memory: 10.1 MB (68.45%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int left=0, right=matrix.size()-1;
+        int top=0, bottom=matrix.size()-1;
 
-        while(left<right) {
+        while(top<bottom) {
+            int left=top;
+            int right=bottom;
+            
             for(int i=0; i<right-left; i++) {
-                int top=left, bottom=right;
-                int topLeft=matrix[top][left+i];
+                int firstEle=matrix[top][left+i];
 
                 matrix[top][left+i]=matrix[bottom-i][left];
+
                 matrix[bottom-i][left]=matrix[bottom][right-i];
+
                 matrix[bottom][right-i]=matrix[top+i][right];
-                matrix[top+i][right]=topLeft;
+
+                matrix[top+i][right]=firstEle;
             }
-            left++;
-            right--;
+
+            top++;
+            bottom--;
         }
 
         return;
