02140a8b1216111789755c3c8334483ca3ff1245
Kush <156246549+kushvash@users.noreply.github.com>
2026-02-28 12:26:29 -0800

    Time: 0 ms (100.00%) | Memory: 21.6 MB (89.01%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> st;
         vector<int> res;
 
         for(int asteroid: asteroids) {
-            while(!st.empty() && st.top()>0 && asteroid<0) {
-                int diff=st.top()+asteroid;
-
-                if(diff>0) {
+            while(asteroid<0 && !res.empty() && res.back()>0) {
+                if(-asteroid<res.back()) {
                     asteroid=0;
-                }else if(diff<0) {
-                    st.pop();
+                }else if(-asteroid>res.back()) {
+                    res.pop_back();
                 }else {
+                    res.pop_back();
                     asteroid=0;
-                    st.pop();
                 }
             }
 
             if(asteroid!=0) {
-                st.push(asteroid);
+                res.push_back(asteroid);
             }
         }
-
-        while(!st.empty()) {
-            res.push_back(st.top());
-            st.pop();
-        }
-
-        reverse(res.begin(), res.end());
 
         return res;
     }
