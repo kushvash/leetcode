820f334f1a0737ee1dd0e0d78ac97355afcc5ca3
Kush <156246549+kushvash@users.noreply.github.com>
2026-03-01 22:23:29 -0800

    Time: 1 ms (15.09%) | Memory: 10.5 MB (73.58%) - LeetSync

---
+++ 293-flip-game/flip-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> generatePossibleNextMoves(string currentState) {
+        vector<string> res;
+        int n=currentState.size();
+
+        for(int i=0; i<n-1; i++) {
+            if(currentState[i]=='+' && currentState[i+1]=='+') {
+                currentState[i]='-';
+                currentState[i+1]='-';
+                res.push_back(currentState);
+                currentState[i]='+';
+                currentState[i+1]='+';
+            }
+        }
+
+        return res;
+    }
+};
