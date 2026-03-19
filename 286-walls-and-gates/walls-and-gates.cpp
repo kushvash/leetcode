c173420d61130a9ced7bdff9231c115a4619dd8e
Kush <156246549+kushvash@users.noreply.github.com>
2026-03-18 17:49:44 -0700

    Time: 91 ms (7.96%) | Memory: 59.3 MB (6.32%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.cpp
+++ 286-walls-and-gates/walls-and-gates.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
-    void wallsAndGates(vector<vector<int>>& rooms) {
-        if (rooms.empty() || rooms[0].empty()) return;
+    void bfs(vector<vector<int>>& rooms, int i, int j) {
+        vector<vector<int>> dirs={
+            {-1, 0}, {1, 0}, {0, -1}, {0, 1}
+        };
 
-        int m = rooms.size(), n = rooms[0].size();
-        queue<pair<int, int>> q;
+        queue<vector<int>> q;
 
-        // Step 1: Add all gates (0s) to the queue
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (rooms[i][j] == 0) {
-                    q.push({i, j});
-                }
-            }
-        }
+        q.push({i, j, 0});
 
-        // Step 2: Multi-source BFS
-        vector<vector<int>> dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-        while (!q.empty()) {
-            auto [x, y] = q.front();
+        while(!q.empty()) {
+            int x=q.front()[0], y=q.front()[1], dist=q.front()[2];
             q.pop();
 
-            for (auto& dir : dirs) {
-                int nx = x + dir[0], ny = y + dir[1];
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n && rooms[nx][ny] == INT_MAX) {
-                    rooms[nx][ny] = rooms[x][y] + 1;
-                    q.push({nx, ny});
+            for(auto& dir: dirs) {
+                int xNew=x+dir[0], yNew=y+dir[1];
+
+                if(xNew>=0 && xNew<rooms.size() && yNew>=0 && yNew<rooms[0].size() && rooms[xNew][yNew]!=-1) {
+                    if(dist+1<rooms[xNew][yNew]) {
+                        rooms[xNew][yNew]=dist+1;
+                        q.push({xNew, yNew, dist+1});
+                    }
                 }
             }
         }
+    }
+
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        int m=rooms.size(), n=rooms[0].size();
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(rooms[i][j]==0) {
+                    bfs(rooms, i, j);
+                }
+            }
+        }
+
+        return;
     }
 };
