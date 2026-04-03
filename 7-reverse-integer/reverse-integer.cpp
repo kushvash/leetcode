8be88e5079d788cea158f1b3dcc90b8562fd756e
Kush <156246549+kushvash@users.noreply.github.com>
2026-04-03 16:09:32 -0700

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.10%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -7,7 +7,7 @@ public:
             int pop=x%10;
             x/=10;
             
-            if (rev>INT_MAX / 10 || (rev==INT_MAX/10 && pop>7))
+            if (rev>INT_MAX/10 || (rev==INT_MAX/10 && pop>7))
                 return 0;
             if (rev<INT_MIN/10 || (rev==INT_MIN/10 && pop<-8))
                 return 0;
